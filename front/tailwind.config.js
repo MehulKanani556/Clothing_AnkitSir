0719d16a478e72cf8c5208ab72c20b046b2a5574
jaykalathiya2005 <jaykalathiyainfotech@gmail.com>
2026-04-14 16:11:17 +0530

    color

--- front/tailwind.config.js
+++ front/tailwind.config.js
@@ -4,7 +4,42 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#14372F",
+        secondary: "#1D5356",
+        gold: "#D4AF37",
+        mainBG: "#F8F9FA",
+        background: "#FFFFFF",
+        border: "#E9ECEF",
+        dark: "#1B1B1B",
+        mainText: "#343A40",
+        lightText: "#ADB5BD",
+      },
+      screens: {
+        xs: "320px",
+        sm375: "375px",
+        sm: "425px",
+        md600: "601px",
+        md: "768px",
+        lg: "1024px",
+        xl: "1280px",
+        "2xl": "1440px",
+        "3xl": "1920px",
+        "4xl": "2560px",
+      },
+      container: {
+        center: true, // Center the container by default
+        screens: {
+          sm: '420px',
+          md: '768px',
+          lg: '1024px',
+          xl: '1280px',
+          '2xl': '1536px',
+          '3xl': '1800px',
+        },
+      },
+    },
   },
   plugins: [],
 }
